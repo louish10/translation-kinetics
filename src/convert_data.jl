bcecd97715aa577ac94fc86de49d519821e71eed
Louis Headley <louish10@hotmail.co.uk>
2021-06-16 02:11:46 +0100

    Add half lives to data

--- src/convert_data.jl
+++ src/convert_data.jl
@@ -10,12 +10,16 @@ select!(
     "mRNA copy number average [molecules/cell]" => "mNumber",
     "transcription rate (vsr) average [molecules/(cell*h)]" => "alpha",
     "translation rate constant (ksp) average [molecules/(mRNA*h)]" => "beta",
+    "mRNA half-life average [h]" => "mHalfLife",
+    "Protein half-life average [h]" => "pHalfLife",
 )
 
 replace!(df.pNumber, "NA" => "")
 replace!(df.mNumber, "NA" => "")
 replace!(df.alpha, "NA" => "")
 replace!(df.beta, "NA" => "")
+replace!(df.mHalfLife, "NA" => "")
+replace!(df.pHalfLife, "NA" => "")
 #df["alpha"] = parse.([Float64], df[!, "alpha"])
 #df["beta"] = parse.([Float64], df[!, "beta"])
 
